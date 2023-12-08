6b0de8a71e74aa2894529ed42ee04eaf1c0aee5a
Bidyuth <bidyuth2019@gmail.com>
2023-12-09 00:00:40 +0600

    show milestone function created)

--- js/main.js
+++ js/main.js
@@ -6,12 +6,12 @@ function loadMilestone() {
     const milestones = document.querySelector('.milestones');
 
     milestones.innerHTML = `${milestoneData.map(function(milestone){
-          
+          console.log(milestone._id);
         return `
         <div class="milestone border-b">
         <div class="flex">
           <div class="checkbox"><input type="checkbox" /></div>
-          <div onclick="openMilestone(this)">
+          <div onclick="openMilestone(this, ${milestone._id})">
             <p>
               ${milestone.name}
               <span><i class="fas fa-chevron-down"></i></span>
@@ -35,16 +35,30 @@ function loadMilestone() {
     }).join('')}`
 }
 
-function openMilestone(milestoneElement){
+function openMilestone(milestoneElement,id){
       const currentPanel = milestoneElement.parentNode.nextElementSibling;
-      console.log(currentPanel);
+    //   console.log(currentPanel);
+      const activePanel = document.querySelector('.active');
       const shownPanel = document.querySelector('.show');
-
+    
+      if(!milestoneElement.classList.contains('active') && activePanel){
+        activePanel.classList.remove('active');
+      }
+      milestoneElement.classList.toggle('active');
+      console.log(milestoneElement);
       if(!currentPanel.classList.contains('show') && shownPanel){
             shownPanel.classList.remove('show');
       }
       
       currentPanel.classList.toggle('show');
+      console.log(id);
+
+      showMilestone(id);
 }
 
+function showMilestone(id){
+    const milestoneImage = document.querySelector('.milestoneImage');
+    milestoneImage.src = milestoneData[id].image;
+       console.log(milestoneData[id].image);
+}
 loadMilestone();
