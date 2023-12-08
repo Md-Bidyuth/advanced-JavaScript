dc15a36bbd56b3d42aa8e8dbb050b8beceeca3f0
Bidyuth <bidyuth2019@gmail.com>
2023-12-08 20:16:11 +0600

    milestone loaded

--- js/main.js
+++ js/main.js
@@ -1,2 +1,38 @@
 const milestoneData = JSON.parse(data).data;
-// console.log(milestoneData);
+console.log(milestoneData);
+
+// load milestone data
+function loadMilestone() {
+    const milestones = document.querySelector('.milestones');
+
+    milestones.innerHTML = `${milestoneData.map(function(milestone){
+          
+        return `
+        <div class="milestone border-b">
+        <div class="flex">
+          <div class="checkbox"><input type="checkbox" /></div>
+          <div>
+            <p>
+              ${milestone.name}
+              <span><i class="fas fa-chevron-down"></i></span>
+            </p>
+          </div>
+        </div>
+        <div class="hidden_panel">
+          ${
+            milestone.modules.map(function(module){
+                console.log(module.name);
+                return `
+                <div class="module border-b">
+                <p>${module.name}</p>
+                 </div>
+                `;
+            }).join("")
+          }
+        </div>
+      </div>
+        `
+    }).join('')}`
+}
+
+loadMilestone();
