de6919a948641e01e213342fbae4918867c8d0e2
Bidyuth <bidyuth2019@gmail.com>
2023-12-10 02:47:53 +0600

    bom part 1 is done

--- bom1.js
+++ bom1.js
@@ -1,5 +1,14 @@
+let myWindow;
 const width = document.getElementById('width');
 const height = document.getElementById('height');
 
 width.innerHTML = window.innerWidth;
 height.innerHTML = window.innerHeight;
+
+function openWindow(){
+    myWindow = window.open('https://google.com', '_blank');
+}
+
+function closeWindow(){
+     myWindow.close();
+}
