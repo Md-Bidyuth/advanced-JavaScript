d1a49ddae156a4f5ecb3500b30a2d8ac706dd2cd
Bidyuth <bidyuth2019@gmail.com>
2023-12-13 13:42:41 +0600

    js window object explored

--- js/bom1.js
+++ js/bom1.js
@@ -1,14 +1,17 @@
-let myWindow;
+
+
+
 const width = document.getElementById('width');
 const height = document.getElementById('height');
+const availWidth = document.getElementById('availWidth');
+const availHeight = document.getElementById('availHeight');
+const colorDepth = document.getElementById('colorDepth');
+const pixelDepth = document.getElementById('pixelDepth');
 
-width.innerHTML = window.innerWidth;
-height.innerHTML = window.innerHeight;
+width.innerText = screen.width;
+height.innerText = screen.height;
+availWidth.innerText = screen.availWidth;
+availHeight.innerText = screen.availHeight;
+colorDepth.innerText = screen.colorDepth;
+pixelDepth.innerText = screen.pixelDepth;
 
-function openWindow(){
-    myWindow = window.open('https://google.com', '_blank');
-}
-
-function closeWindow(){
-     myWindow.close();
-}
